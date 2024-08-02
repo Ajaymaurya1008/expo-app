ef105db7ed5aef4d2b1e7f725d2ce74c3cedab2a
Ajaymaurya1008 <ajaykvmaurya@gmail.com>
2024-08-02 11:25:09 +0530

    Add initial configuration files and setup project structure

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { Text, View } from "react-native";
+import {
+  Button,
+  ButtonText,
+  ButtonSpinner,
+  ButtonIcon,
+  ButtonGroup,
+} from "@/components/ui/button";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button size="md" variant="solid" action="primary">
+        <ButtonText>Hello World!</ButtonText>
+      </Button>
+    </View>
+  );
+}
