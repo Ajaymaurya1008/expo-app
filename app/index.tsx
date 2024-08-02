68f7f278734963fe0a2d54bdd2fe97f1acef9bb6
sra1kumar-NULL <sravansandeep163@gmail.com>
2024-08-02 12:48:50 +0530

    fix: components update

--- app/index.tsx
+++ app/index.tsx
@@ -6,6 +6,8 @@ import {
   ButtonIcon,
   ButtonGroup,
 } from "@/components/ui/button";
+import { Box } from "@/components/ui/box";
+import { HStack } from "@/components/ui/hstack";
 
 export default function Index() {
   return (
@@ -14,12 +16,22 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 10,
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Button size="md" variant="solid" action="primary">
         <ButtonText>Hello World!</ButtonText>
       </Button>
+      <Text
+        onPress={() => {
+          console.log("Thising the on press");
+        }}
+        className="bg-blue-600 px-4 py-2 color-white rounded-md"
+      >
+        CLICK
+      </Text>
+      <Box className="bg-red-500 w-56 h-56 rounded-lg"></Box>
     </View>
   );
 }
